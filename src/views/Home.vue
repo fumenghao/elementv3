50ac4304322cf7aff3bc3eccf56bcfa5d8844899
fuhao <fmhjasper@163.com>
2021-10-28 17:59:32 +0800

    修改代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <el-button>I am ElButton</el-button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
